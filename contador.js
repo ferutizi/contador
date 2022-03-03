192d3d79091f6b8e94e841d6cd4fb2273eed479f
ferutizi <fer.utizi@gmail.com>
2022-03-03 13:14:49 -0300

    agrego boton reset, +10 y x2

--- contador.js
+++ contador.js
@@ -1,6 +1,9 @@
 
 const btnAumentar = document.querySelector('.btn-primary');
 const btnDisminuir = document.querySelector('.btn-danger');
+const btn10 = document.querySelector('.btn10');
+const reset = document.querySelector('.btnReset');
+const btn2 = document.querySelector('.btn2');
 
 const span = document.getElementById('span');
 
@@ -13,5 +16,20 @@ btnAumentar.addEventListener('click', () => {
 
 btnDisminuir.addEventListener('click', () => {
     contador--;
+    span.textContent = contador;
+})
+
+btn10.addEventListener('click', () => {
+    contador = contador + 10;
+    span.textContent = contador;
+})
+
+btn2.addEventListener('click', () => {
+    contador = contador * 2;
+    span.textContent = contador;
+})
+
+reset.addEventListener('click', () => {
+    contador = 0;
     span.textContent = contador;
 })
